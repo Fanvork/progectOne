481f466f492977b76772c63691cd4aff483c2505
Fanvork <63975662+Fanvork@users.noreply.github.com>
2020-04-19 22:08:27 +0300

    Add files via upload

---
+++ lul/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool prost = true;
+            Console.WriteLine("Введите число\n");
+            int n = Int32.Parse(textBox1.Text);
+
+            for (int i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    prost = false;
+                    break;
+                }
+            }
+            if (prost)
+            {
+                MessageBox.Show("Число простое");
+
+            }
+            else
+            {
+                MessageBox.Show("Число не простое");
+            }
+            
+        }
+    }
+}
